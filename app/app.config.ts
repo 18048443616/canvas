a8546f050f5ff3be2c6d46f8e2bdf1c52e8b34a1
twj <74292630+18048443616@users.noreply.github.com>
2025-10-15 11:42:27 +0800

    1 change by Nuxt Studio

--- app/app.config.ts
+++ app/app.config.ts
@@ -18,7 +18,7 @@ export default defineAppConfig({
     spotify: 'https://open.spotify.com/user/yuvl0zpp3bpx4hne1ag7huten?si=df7ee2777c0c4fc4',
   },
   seo: {
-    title: 'Canvas a Nuxt portfolio template',
+    title: 'demo测试',
     description: 'Canvas is a simple but beautiful portfolio template for designers and developers built with Nuxt and Tailwind CSS. Made with ❤️ by HugoRCD',
     url: 'https://canvas.hrcd.fr',
   },
